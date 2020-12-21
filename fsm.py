e19df88e6ac4b41aff8c32e4b37c90812585c3a6
Dexamonchrome <gumagante123@gmail.com>
2020-12-21 13:53:11 +0800

    tr

--- fsm.py
+++ fsm.py
@@ -150,6 +150,7 @@ class TocMachine(GraphMachine):
         print("I'm entering Iphone 12")
         reply_token = event.reply_token
         text = "Please choose which information you want to know!"
+        send_text_message(reply_token, text)
         send_IP12_carousel(reply_token)
 
     def on_enter_IP12_Price(self, event):
